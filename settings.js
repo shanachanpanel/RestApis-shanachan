407218da2f4dd864850805a385b7814373acd08f
shanachan <77118749+kap320@users.noreply.github.com>
2023-06-28 13:43:43 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://boyahmad13131:X^:Bb^+q^zRA4TC@cluster8.cobowvl.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "rizaedtz01@gmail.com" //email
+global.email_password = "Bb^+q^zRA4TC" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
